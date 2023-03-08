a528c728080f89fa76070397f742078c52ff86cf
engageintellect <jc9361@gmail.com>
2023-03-07 22:30:27 -0800

    cleaning up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
           </Head>
 
           <body>
+            {/* TODO: THIS IS SKETCHY */}
             <script
               dangerouslySetInnerHTML={{
                 __html: `
